06d09cfa7c2e3440aaa9d42ad34570ace6ea1f83
Aliyu Abubakar <aliyuag44@gmail.com>
2022-08-10 12:56:28 +0530

    php

---
+++ about.php
@@ -0,0 +1,155 @@
+<?php
+    include 'header.php';
+?>
+
+            <div class="links-container">
+                <ul>
+                <li><a href="/" class="links">Home</a></li>
+                    <li><a href="about.php" class="links active">About Us</a></li>
+                    <li><a href="services.php" class="links">Services</a></li>
+                    <li><a href="programs.php" class="links">Programmes</a></li>
+                    <li><a href="events.php" class="links">Events</a></li>
+                    <li><a href="ideas-insight.php" class="links">Ideas & Insights</a></li>
+                    <li><a href="contact.php" class="links">Contact Us</a></li> 
+                </ul>
+            </div>
+        </div>
+    </header>
+
+    <section class="about-header">
+        <h1>About Us</h1>
+    </section>
+
+    <section class="about-container">
+        <div class="about-heading">
+            <h2>Our mission is clear</h2>
+            <p>To gain understanding and muster the courage to take action, we delve deeply. We assemble the ideal group of individuals to challenge conventional wisdom and spur change. We collaborate with our clients to develop the skills necessary for firms to gain a sustainable competitive advantage. Together, we are influencing the future.</p>
+        </div>
+        <div class="main-about">
+            <div class="main-item">
+                <div class="bg"></div>
+                <h2 class="bg-text">We Innovate</h2>
+                <p class="bg-text">Plans for businesses may have an internal or external focus. Plans with an external focus prioritise objectives that are significant to external stakeholders, particularly those with a financial stake.</p>
+            </div>
+            <div class="main-item sec">
+                <h2>Our Performance</h2>
+                <p>We are pleased to provide our clients with highest quality of performance and customer service. Our team is dedicated to meeting and exceeding your expectation.</p>
+            </div>
+            <div class="main-item thd">
+                <h3>Clients</h3>
+                <div class="contents">
+                    <img src="img/Clients/Anglogold.PNG" alt="">
+                    <img src="img/Clients/Ghana Navy.PNG" alt="">
+                    <img src="img/Clients/Cocacola.PNG" alt="">
+                    <img src="img/Clients/Conship.PNG" alt="">
+                    <img src="img/Clients/Enterprise.PNG" alt="">
+                    <img src="img/Clients/GHACEM.PNG" alt="">
+                    <img src="img/Clients/GIZ.PNG" alt="">
+                    <img src="img/Clients/NPA.JPG" alt="">
+                    <img src="img/Clients/Stanchart.JPG" alt="">
+                    <img src="img/Clients/UG.PNG" alt="">
+                </div>
+            </div>
+            <div class="main-item fth"></div>
+        </div>
+    </section>
+
+    <section class="board">
+        <div class="leadership">
+            <h2>Our Leadership</h2>
+            <div class="note">
+                <p>Our workforce enables our operations. They represent the integrity of what I&YA stands for because they are constantly working on the front lines with our client-partners. As a result, we have witnessed many of our employees advance fast through the ranks and build their own reputations in their local communities.</p>
+                <p>Ishmael Yamson & Associates takes seriously its duties to uphold employment equity and provide training. AES makes significant investments in the development of outstanding individuals and their equal opportunity advancement into the world of business operations.</p>
+            </div>
+            <div class="leaders">
+                <div class="item">
+                    <img src="img/hero/2.jpg" alt="">
+                    <div class="details">
+                        <h4>Stanley John</h4>
+                        <h5>Former Chairman</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum odit quis, inventore eaque distinctio fugit! Tempore optio hic officia laudantium. Atque consequuntur deleniti dicta illo laboriosam provident iusto autem iste.</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="img/hero/2.jpg" alt="">
+                    <div class="details">
+                        <h4>Stanley John</h4>
+                        <h5>Former Chairman</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum odit quis, inventore eaque distinctio fugit! Tempore optio hic officia laudantium. Atque consequuntur deleniti dicta illo laboriosam provident iusto autem iste.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="directors">
+            <h3>Directors</h3>
+            <div class="director-container">
+                <div class="dir-item">
+                    <img src="img/hero/3.jpg" alt="">
+                    <h4>Aliyu Amir</h4>
+                    <p>Designer</p>
+                </div>
+                <div class="dir-item">
+                    <img src="img/hero/3.jpg" alt="">
+                    <h4>Aliyu Amir</h4>
+                    <p>Designer</p>
+                </div>
+                <div class="dir-item">
+                    <img src="img/hero/3.jpg" alt="">
+                    <h4>Aliyu Amir</h4>
+                    <p>Designer</p>
+                </div>
+                <div class="dir-item">
+                    <img src="img/hero/3.jpg" alt="">
+                    <h4>Aliyu Amir</h4>
+                    <p>Designer</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="about-container">
+        <div class="about-note">
+            <div class="about-note-ceo">
+                <img src="img/hero/manager.jpeg" alt="">
+                <h5>Chairman</h5>
+                <h3>Omar Taufiq</h3>
+                <p>Other people's successes are good news - for them and for you. Good for you because they show you a way to go.</p>
+            </div>
+            <div class="about-note-container">
+                <div class="about-note-item">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quod corporis, possimus nihil natus, a doloremque impedit maxime id vel adipisci eaque non, cum earum quaerat totam rerum tenetur eos?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quod corporis, possimus nihil natus, a doloremque impedit maxime id vel adipisci eaque non, cum earum quaerat totam rerum tenetur eos?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quod corporis, possimus nihil natus, a doloremque impedit maxime id vel adipisci eaque non, cum earum quaerat totam rerum tenetur eos?</p>
+                </div>
+                <div class="about-note-item">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quod corporis, possimus nihil natus, a doloremque impedit maxime id vel adipisci eaque non, cum earum quaerat totam rerum tenetur eos?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quod corporis, possimus nihil natus, a doloremque impedit maxime id vel adipisci eaque non, cum earum quaerat totam rerum tenetur eos?</p>
+                    <a href="#">Join the conversation</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="call-back bg">
+        <div class="call-back-heading">
+            <h2>Let's help you!</h2>
+        </div>
+        <div class="call-back-container">
+            <div class="call-back-icon about">
+                <img src="img/icons/customer-service-agent.png" alt="">
+                <h3>Phone: 800-454-9272</h3>
+                <p>Working from home meant we could vary snack and coffee breaks, change our desks or…</p>
+                <a href="#">Our Address</a>
+            </div>
+            <form action="">
+                <input type="text" name="name" id="" placeholder="Name">
+                <input type="text" name="pnumber" id="" placeholder="Phone Number (with country code)">
+                <textarea class="textarea" name="" placeholder="Describe Briefly"></textarea>
+                <button class="normal">Send</button>
+            </form>
+        </div>
+    </section>
+
+    <?php
+        include 'footer.php';
+    ?>
---
+++ programs.php
@@ -0,0 +1,87 @@
+<?php
+    include 'header.php';
+?>
+            <div class="links-container">
+                <ul>
+                    <li><a href="/" class="links">Home</a></li>
+                    <li><a href="about.php" class="links">About Us</a></li>
+                    <li><a href="services.php" class="links">Services</a></li>
+                    <li><a href="programs.php" class="links active">Programmes</a></li>
+                    <li><a href="events.php" class="links">Events</a></li>
+                    <li><a href="ideas-insight.php" class="links">Ideas & Insights</a></li>
+                    <li><a href="contact.php" class="links">Contact Us</a></li> 
+                </ul>
+            </div>
+        </div>
+    </header>
+
+    <section class="programs-header">
+        <h1>Programmes</h1>
+        <p>Founded in 2001 in New York, USA</p>
+    </section>
+
+    <section class="programs-container">
+        <h4>Core Services</h4>
+        <div class="core-item-container">
+            <div class="core-item">
+                <img src="img/icons/agreement.png" alt="">
+                <h2>Strategy and Planning</h2>
+                <p>A business strategy is the means by which it sets out to achieve its desired ends.  </p>
+                <ul>
+                    <li>Executive summary</li>
+                    <li>Business description</li>
+                    <li>Environment analysis</li>
+                    <li>Industry background</li>
+                    <li>Competitor analysis</li>
+                </ul>
+            </div>
+            <div class="core-item">
+                <img src="img/icons/task.png" alt="">
+                <h2>Strategy and Planning</h2>
+                <p>A business strategy is the means by which it sets out to achieve its desired ends.  </p>
+                <ul>
+                    <li>Executive summary</li>
+                    <li>Business description</li>
+                    <li>Environment analysis</li>
+                    <li>Industry background</li>
+                    <li>Competitor analysis</li>
+                </ul>
+            </div>
+            <div class="core-item">
+                <img src="img/icons/bars.png" alt="">
+                <h2>Strategy and Planning</h2>
+                <p>A business strategy is the means by which it sets out to achieve its desired ends.  </p>
+                <ul>
+                    <li>Executive summary</li>
+                    <li>Business description</li>
+                    <li>Environment analysis</li>
+                    <li>Industry background</li>
+                    <li>Competitor analysis</li>
+                </ul>
+            </div>
+            <div class="core-item">
+                <img src="img/icons/save-money.png" alt="">
+                <h2>Strategy and Planning</h2>
+                <p>A business strategy is the means by which it sets out to achieve its desired ends.  </p>
+                <ul>
+                    <li>Executive summary</li>
+                    <li>Business description</li>
+                    <li>Environment analysis</li>
+                    <li>Industry background</li>
+                    <li>Competitor analysis</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <section class="call-back bg ser">            
+        <div class="call-serv">
+            <h1>Let's help you!</h1>
+            <p>Working from home meant we could vary snack and coffee breaks…</p>
+        </div>
+        <a href="contact.php" class="normal">Contact Us</a>
+    </section>
+
+    <?php
+        include 'footer.php';
+    ?>
